6f7ed2689a7f731454207dbb002225dd4302c9ec
Otavio Cordeiro <otavio@horadomac.com>
2012-12-07 16:08:47 -0200

    Fixed NibName.

--- NHCalendarActivityDemo/NHCalendarActivityDemo/AppDelegate.m
+++ NHCalendarActivityDemo/NHCalendarActivityDemo/AppDelegate.m
@@ -34,7 +34,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.viewController = [[DemoViewController alloc] initWithNibName:@"NoiteHojeViewController"
+    self.viewController = [[DemoViewController alloc] initWithNibName:@"DemoViewController"
                                                                     bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
